6c0720a570c319dcfaf4b335c715a02347bd51f8
dustin song <dustinsong@outlook.com>
2018-01-27 15:41:45 +0800

    create microProgram

---
+++ components/goods.js
@@ -0,0 +1,37 @@
+Component({
+  
+  properties: {
+    goodsUrl: {
+      type: String,
+      value: "images/default/goods-default.png"
+    },
+    goodsName: {
+      type: String,
+      value: "暂未起名"
+    },
+    goodsDesc: {
+      type: String,
+      value: ""
+    },
+    goodsPrice: {
+      type: Number,
+      value: 0
+    },
+    goodsNum: {
+      type: Number,
+      value: 0
+    },
+    goodsUnit: {
+      type: String,
+      value: "两"
+    }
+  },
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+})
