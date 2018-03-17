fa5df3af0c385088f2b3c2484bef917e53e9380d
Dustin Song <DustinSong@outlook.com>
2018-03-17 16:28:00 +0800

    我的页面

---
+++ components/address/address.js
@@ -0,0 +1,38 @@
+// components/address/address.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    username:{
+      type: String,
+      value: "空姓名"
+    },
+    phone:{
+      type: String,
+      value: "130xxxx7754"
+    },
+    address:{
+      type: String,
+      value: "默认"
+    },
+    isDefault: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
